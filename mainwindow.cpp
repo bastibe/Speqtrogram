eb7488bf8b19fb503876ac49d01aa5200a2ebf7a
Bastian Bechtold <basti@bastibe.de>
2013-01-30 22:01:40 +0100

    Pre-select the default API and device.
    Including the period.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,6 +15,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->hostApiList->setModel(new HostApiList());
     ui->deviceList->setModel(new DeviceList());
 
+    // Pre-select the default API
+    ui->hostApiList->setCurrentIndex(Pa_GetDefaultHostApi());
+    static_cast<DeviceList*>(ui->deviceList->model())->updateHostApi(Pa_GetDefaultHostApi());
+
+    // Pre-select the default device for the given API
+    const PaDeviceIndex first_device = Pa_HostApiDeviceIndexToDeviceIndex(Pa_GetDefaultHostApi(), 0);
+    ui->deviceList->setCurrentIndex(Pa_GetDefaultOutputDevice()-first_device);
+
     connect(ui->hostApiList, SIGNAL(currentIndexChanged(int)),
             ui->deviceList->model(), SLOT(updateHostApi(int)) );
 }
