c5dc346dcb5ee822fbae5d676983bcf86ec745cd
Bastian Bechtold <basti@bastibe.de>
2013-04-17 23:14:08 +0200

    Implement rudimentary waveform plotting.
    Added QGraphicsView instead of QWidget for waveformNavigator and
    implemented waveform plotting.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,9 @@
 #include "hostapilist.h"
 #include <QFileDialog>
 #include <QFileInfo>
+#include <QDebug>
+#include <QGraphicsPathItem>
+#include "sndfile.hh"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -60,6 +63,38 @@ void MainWindow::open()
                                                     tr("Sound Files (*.wav *.flac *.aiff *.ogg)"));
     QDir::setCurrent(QFileInfo(fileName).absolutePath());
     player.openSoundFile(fileName.toStdString());
+
+    QGraphicsScene *waveformNavigatorScene = new QGraphicsScene();
+    QPainterPath waveformMax(QPointF(0,0));
+    QPainterPath waveformMin(QPointF(0,0));
+    SndfileHandle soundFile(fileName.toStdString());
+    if (soundFile.error()) {
+        qDebug() << soundFile.strError();
+    }
+    sf_count_t framesPerPixel = soundFile.frames()/ui->waveformNavigator->width();
+    float samples[framesPerPixel*soundFile.channels()];
+    sf_count_t totalFrames = 0;
+    while (soundFile.readf(samples, framesPerPixel) != 0) {
+        float maximum = 0;
+        float minimum = 0;
+        for (int f=0; f<framesPerPixel; f++) {
+            for (int c=0; c<soundFile.channels(); c++) {
+                maximum = samples[f*c+c] > maximum ? samples[f*c+c] : maximum;
+                minimum = samples[f*c+c] < minimum ? samples[f*c+c] : minimum;
+            }
+        }
+        waveformMax.lineTo(totalFrames/framesPerPixel, maximum*ui->waveformNavigator->height()*0.5);
+        waveformMin.lineTo(totalFrames/framesPerPixel, minimum*ui->waveformNavigator->height()*0.5);
+        totalFrames += framesPerPixel;
+    }
+    QColor lightBlue = QColor(Qt::blue);
+    lightBlue.setAlphaF(0.5);
+    waveformNavigatorScene->addPath(waveformMax, QPen(Qt::blue), QBrush(lightBlue));
+    waveformNavigatorScene->addPath(waveformMin, QPen(Qt::blue), QBrush(lightBlue));
+    ui->waveformNavigator->setScene(waveformNavigatorScene);
+    ui->waveformNavigator->setRenderHint(QPainter::Antialiasing, true);
+
+    qDebug() << ui->waveformNavigator->sceneRect();
 }
 
 void MainWindow::changePlayButtonState(bool isPlaying)
